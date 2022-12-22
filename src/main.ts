be0791f575939c0a909b2d99b0142b9060370bad
Piotr Czeglik <piotr.czeglik@iohk.io>
2022-12-22 15:17:03 +0100

    feat: cardano services fetch token metadata

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
-function hello(name: string) {
-  console.log("Hello, " + name + "!");
-}
+import { assetInfoHttpProvider } from "@cardano-sdk/cardano-services-client";
+import { AssetProvider, Cardano, Asset } from "@cardano-sdk/core";
 
-hello("TypeScript");
+const CARDANO_SERVICES_URL_MAINNET =
+  "https://backend.live-mainnet.eks.lw.iog.io";
+
+async function main() {
+  const httpProviderConfig = (baseUrl: string) => ({
+    baseUrl,
+    logger: console,
+  });
+
+  const assetProvider: AssetProvider = assetInfoHttpProvider(
+    httpProviderConfig(`${CARDANO_SERVICES_URL_MAINNET}/asset`)
+  );
+
+  const assetName = "4d494e";
+  const assetPolicyId =
+    "29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6";
+
+  const assetId = Cardano.AssetId(`${assetPolicyId}${assetName}`);
+
+  const tokenMetadata = await assetProvider.getAsset({
+    assetId: assetId,
+    extraData: {
+      tokenMetadata: true,
+    },
+  });
+
+  return tokenMetadata;
+}
+main().then((result) => console.log(result));
